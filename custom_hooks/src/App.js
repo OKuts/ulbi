ddd8806495dc1e64e801516eb0f0ee134a3fd2d3
Ol_Kuts <kuts.ol.vas@gmail.com>
2021-10-05 13:02:26 +0300

    rename folder

---
+++ custom_hooks/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Hover} from "./components/Hover";
+import {Input} from "./components/Input";
+import {Search} from "./components/Search";
+
+function App() {
+  return (
+    <>
+      <Input/>
+      <Hover/>
+      <Search/>
+    </>
+  );
+}
+
+export default App;
